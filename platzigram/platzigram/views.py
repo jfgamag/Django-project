5ac70a0cfd536a9c6b4a4dc4c93da214a06efb63
Juan Gama <juangamag93@gmail.com>
2021-08-05 13:00:03 -0500

    Returns current server time

--- platzigram/platzigram/views.py
+++ platzigram/platzigram/views.py
@@ -1,4 +1,15 @@
 from django.http.response import HttpResponse
+from datetime import date, datetime
+
 
 def hello_world(request):
-    return HttpResponse('Hello world')
+    """Returns a greeting
+
+    Args:
+        request (HTTP):  Receives HTTP request
+
+    Returns:
+        Greeting: returns a greeting
+    """
+    now = datetime.now()
+    return HttpResponse('Oh Hi! Current server time is {now}'.format(now=str(now)))
