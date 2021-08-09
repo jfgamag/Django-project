a87cd365bd843c32d7aa59cf2e2d731f84e874bb
Juan Gama <juangamag93@gmail.com>
2021-08-09 15:54:43 -0500

    Checked spelling mistake in User class and added data to the SQLite DB

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
 
-    fist_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
     bio = models.TextField(blank=True)
