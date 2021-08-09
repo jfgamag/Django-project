7ce6f69a8d65bd79d1bd18d9b578fe223ed08b07
Juan Gama <juangamag93@gmail.com>
2021-08-09 12:18:42 -0500

    Created user class in posts app, updated fields of the class and applied migrations

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
+
+    fist_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
