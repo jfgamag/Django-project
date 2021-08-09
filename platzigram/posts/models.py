f9fa7cefe5b9c867168180e1bdc7046346e90b1d
Juan Gama <juangamag93@gmail.com>
2021-08-09 16:22:36 -0500

    Created country instance in models.py and made migrations to posts app

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
+    country = models.CharField(max_length=30, default='Colombia')
+
+
     bio = models.TextField(blank=True)
     is_admin = models.BooleanField(default=False)
 
