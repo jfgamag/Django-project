4ce4108bbccb4add1dc37f97dad41e294afe931f
Juan Gama <juangamag93@gmail.com>
2021-08-17 14:38:48 -0500

    Modified how the details of a profile are shown using field sets

--- platzigram/users/admin.py
+++ platzigram/users/admin.py
@@ -1,7 +1,11 @@
 """User admin classes
 """
+# Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# Models
 from users.models import Profile
+from django.contrib.auth.models import User
 # Register your models here.
 
 @admin.register(Profile)
@@ -16,9 +20,36 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number', 'website', 'picture')
     #Search fields
     search_fields = (
+        'user__username',
         'user__email', 
         'user__first_name', 
         'user__last_name', 
         'user__phone_number'
         )
+    #Filters for profile
+    list_filter = (
+        'user__username',
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+        )
     
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+    readonly_fields = ('created', 'modified',)
+
+
+
