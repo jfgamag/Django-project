b55be5a8a84f17a5848d24ca14f135eca05f8c71
Juan Gama <juangamag93@gmail.com>
2021-08-18 18:19:32 -0500

    Cerated posts field in admin, changed which objects are editable and which aren't, added search bar and search fields Created posts field in admin, changed which objects are editable and which aren't, added search bar and search fields

--- platzigram/posts/admin.py
+++ platzigram/posts/admin.py
@@ -1,3 +1,33 @@
+#Django
 from django.contrib import admin
+# from django.contrib.auth.models import User
+#models
+from posts.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    """Post admin class"""
+    list_display = ('pk','user', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+    search_fields = (
+        'title',
+        'pk',
+        'user__username'
+    )
+
+    fieldsets = (
+        ('Posts', {
+            'fields': (
+                ('user', 'title'),)
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),)
+        }),
+    )
+    readonly_fields = ('created', 'modified')
+
+    
