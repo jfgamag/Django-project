20e8cb30c9697101ef347b35299b288773c7fa3b
Juan Gama <juangamag93@gmail.com>
2021-08-26 17:31:51 -0500

    Added login for users, error if information doesn' match, templates to show when accessing the server

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -7,12 +7,17 @@ from django.conf import settings
 
 
 from posts import views as posts_views
+from users import views as user_views
+from platzigram import views as local_views
 from users import models as user_m
 
 
 urlpatterns = [ 
-    path('admin/', admin.site.urls),
-    path('posts/', posts_views.list_posts),
-    path('users/', user_m.Profile),
+    path('admin/', admin.site.urls, name='hello_world'),
+    path('sort/', local_views.sort_n, name='sort'),
+    path('hi/', local_views.hello, name='hi'),
+    path('posts/', posts_views.list_posts, name='feed'),
+    path('users/', user_m.Profile, name='profile'),
+    path('users/login', user_views.login_view, name='login'),
     
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
